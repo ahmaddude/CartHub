8c13bb983a524adb2e9b707cb337038a5d799874
ahmad <mk4249796@gmail.com>
2026-07-27 16:03:19 +0300

    fixed dependency error

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
 export const connectdb =async()=>{
     try {
+        if(!process.env.MONGO_URI){
+            console.log("MONGO_URI is not defined!")
+            process.exit(1)
+        }
         const conn=  await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected:${conn.connection.host}` )
     } catch (error) {
